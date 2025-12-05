368e6571d075b7d2e5df4cefc9952f5190dad1a1
DagnoX <dagnox@gmail.com>
2025-12-05 14:21:59 -0600

    mensaje del commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,16 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://headroomsound.netlify.app", // Tu URL real
+
   integrations: [mdx(), sitemap(), icon()],
+
+  // ESTA ES LA PARTE PARA QUE CARGUE EN TU CELULAR
+  server: {
+    host: true, // Esto abre el servidor a tu Wi-Fi
+    port: 4321, 
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
